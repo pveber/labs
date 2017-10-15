245b29c21cfafb35e794dba3f3c84b8d7b6ab8fd
Votre Nom <Vous@exemple.com>
2017-10-15 21:21:04 +0200

    bistro: added Chipseq_utils module
    with a function to convert beds to gff

---
+++ bistro/chipseq_utils.ml
@@ -0,0 +1,21 @@
+let%bistro gff_of_bed bed =
+  let open Biocaml_base in
+  let open Labs_biopipes.Biopipes in
+  run (
+    from_file [%dep bed]
+    $$ bed_parser ()
+    $$ map (function
+        | (chr, start_pos, stop_pos, name :: _) ->
+          `record (
+            Gff.record
+              ~strand:`not_applicable
+              ~attributes:["gene_id", [name]]
+              ~feature:"peak"
+              chr start_pos stop_pos
+          )
+        | _ -> failwith "gff_of_bed: expect at least bed4"
+      )
+    $$ gff_unparser `two
+    $$ to_file [%dest]
+  )
+
---
+++ bistro/chipseq_utils.mli
@@ -0,0 +1,4 @@
+open Bistro.Std
+open Bistro_bioinfo.Std
+
+val gff_of_bed : #bed4 workflow -> gff workflow
