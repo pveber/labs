c3c1d641668a34fd9281258326a14d8f651a9d55
Philippe Veber <philippe.veber@gmail.com>
2017-10-17 21:15:42 +0200

    bistro/Chipseq_utils: added gff to TSS bed construction

--- bistro/chipseq_utils.ml
+++ bistro/chipseq_utils.ml
@@ -19,3 +19,24 @@ let%bistro gff_of_bed bed =
     $$ to_file [%dest]
   )
 
+let%bistro tss_bed_of_gff ~upstream ~downstream gff =
+  let open Biocaml_base in
+  let open Labs_biopipes.Biopipes in
+  run (
+    from_file [%dep gff]
+    $$ gff3_parser ()
+    $$ filter_map (function
+        | `Record { Gff.feature = Some "transcript" ; seqname ; start_pos ; stop_pos ; strand } ->
+          let start_pos, stop_pos =
+            match strand with
+            | `Plus -> start_pos - upstream, start_pos + downstream
+            | `Minus -> stop_pos - downstream, stop_pos + upstream
+            | `Not_stranded
+            | `Unknown -> assert false
+          in
+          Some (seqname, start_pos, stop_pos, [])
+        | _ -> None
+      )
+    $$ bed_unparser ()
+    $$ to_file [%dest]
+  )
--- bistro/chipseq_utils.mli
+++ bistro/chipseq_utils.mli
@@ -2,3 +2,8 @@ open Bistro.Std
 open Bistro_bioinfo.Std
 
 val gff_of_bed : #bed4 workflow -> gff workflow
+val tss_bed_of_gff :
+  upstream:int ->
+  downstream:int ->
+  gff workflow ->
+  bed3 workflow
