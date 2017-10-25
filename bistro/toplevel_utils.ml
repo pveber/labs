58d90a26d66f34794c8dfa354da454147c2ea38f
Philippe Veber <philippe.veber@gmail.com>
2017-10-25 18:11:21 +0200

    bistro: added Toplevel_utils module

---
+++ bistro/toplevel_utils.ml
@@ -0,0 +1,35 @@
+open Bistro_utils
+open Bistro.Std
+open Bistro_bioinfo.Std
+
+let with_workflow w ~f =
+  let open Bistro_app in
+  run ~keep_all:true ~np:8 ~mem:(`GB 8) (
+    pure (fun (Bistro_app.Path p) -> f p) $ pureW w
+  )
+
+let path w =
+  with_workflow w ~f:(fun x -> x)
+
+let sh fmt =
+  Printf.kprintf (fun s -> ignore (Sys.command s)) fmt
+
+let rm w =
+  with_workflow w ~f:(fun p ->
+      sh "rm %s" p
+    )
+
+let less (w : #text_file workflow) =
+  with_workflow w ~f:(fun p ->
+      sh "less %s" p
+    )
+
+let browse w =
+  with_workflow w ~f:(fun p ->
+      sh "firefox --no-remote %s" p
+    )
+
+let ls (w : _ #directory workflow) =
+  with_workflow w ~f:(fun p ->
+      sh "ls %s" p
+    )
---
+++ bistro/toplevel_utils.mli
@@ -0,0 +1,7 @@
+open Bistro.Std
+
+val path : _ workflow -> string
+val rm : _ workflow -> unit
+val browse : _ workflow -> unit
+val ls : _ #directory workflow -> unit
+val less : #text_file workflow -> unit
