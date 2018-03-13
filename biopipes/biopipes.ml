1221551992aa942d3da26f90917d7f26598ad07a
Philippe Veber <philippe.veber@gmail.com>
2018-03-13 14:15:41 +0100

    biopipes: added macs xls parser

--- biopipes/biopipes.ml
+++ biopipes/biopipes.ml
@@ -1,5 +1,6 @@
 open Base
 open Biocaml_base
+open Labs_biostuff
 include Pipes_unix.Pipe
 
 let ( %% ) f g = Fn.(flip compose) f g
@@ -26,10 +27,10 @@ let lines_to_strings () =
 
 let bed_parser () =
   lines ()
-  $$ map (Bed.item_of_line %% Result.ok_or_failwith)
+  $$ map (Biocaml_base.Bed.item_of_line %% Result.ok_or_failwith)
 
 let bed_unparser () =
-  map Bed.line_of_item
+  map Biocaml_base.Bed.line_of_item
   $$ lines_to_strings ()
 
 let gff3_parser () =
@@ -48,3 +49,9 @@ let table_parser () =
 let table_unparser () =
   map (fun fields -> Line.of_string_unsafe (String.concat ~sep:"\t" fields))
   $$ lines_to_strings ()
+
+let macs_xls_parser =
+  map Macs.Xls.parse
+
+let macs_xls_unparser =
+  map Macs.Xls.unparse
--- biopipes/biopipes.mli
+++ biopipes/biopipes.mli
@@ -1,4 +1,5 @@
 open Biocaml_base
+open Labs_biostuff
 include module type of Pipes_unix.Pipe
 (* open Pipes_parser *)
 
@@ -6,9 +7,12 @@ include module type of Pipes_unix.Pipe
 
 val lines : unit -> (string, Line.t, unit) t
 val lines_to_strings : unit -> (Line.t, string, unit) t
-val bed_parser : unit -> (string, Bed.item, unit) t
-val bed_unparser : unit -> (Bed.item, string, unit) t
+val bed_parser : unit -> (string, Biocaml_base.Bed.item, unit) t
+val bed_unparser : unit -> (Biocaml_base.Bed.item, string, unit) t
 val gff3_parser : unit -> (string, Gff.item, unit) t
 val gff_unparser : [`two | `three] -> (Gff.item, string, unit) t
 val table_parser : unit -> (string, string list, unit) t
 val table_unparser : unit -> (string list, string, unit) t
+
+val macs_xls_parser : (Line.t, Macs.Xls.item, unit) t
+val macs_xls_unparser : (Macs.Xls.item, string, unit) t
