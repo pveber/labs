fb248dbd43ca7965def4ae7b4b8465d0c9a0b973
Philippe Veber <philippe.veber@gmail.com>
2018-04-24 11:37:38 +0200

    biostuff.GLoc: of_string/to_string

--- biostuff/gLoc.ml
+++ biostuff/gLoc.ml
@@ -8,3 +8,18 @@ type t = {
 
 let to_string { chr ; st ; ed } =
   Printf.sprintf "%s:%d-%d" chr st ed
+
+let of_string_exn s =
+  try Caml.Scanf.sscanf s "%[^:]:%d-%d" (fun chr st ed -> { chr ; st ; ed })
+  with _ -> failwith ("GLoc.of_string_exn: " ^ s)
+
+let%test "of_string_exn_1" =
+  Caml.(
+    of_string_exn "chr1:3053032-3053034"
+    =
+    { chr = "chr1" ; st = 3053032 ; ed = 3053034 }
+  )
+
+let of_string s =
+  try Ok (of_string_exn s)
+  with _ -> Error `Parse_error
--- biostuff/gLoc.mli
+++ biostuff/gLoc.mli
@@ -4,4 +4,6 @@ type t = {
   ed : int ;
 } [@@deriving compare, sexp]
 
+val of_string : string -> (t, [> `Parse_error]) result
+val of_string_exn : string -> t
 val to_string : t -> string
