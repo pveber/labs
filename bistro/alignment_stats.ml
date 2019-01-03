ca959b6eac0bd3dcc24a2e0b83f7d09a37592949
Philippe Veber <philippe.veber@gmail.com>
2019-01-03 14:20:13 +0100

    bistro really doesn't use croquis anymore

--- bistro/alignment_stats.ml
+++ bistro/alignment_stats.ml
@@ -127,8 +127,23 @@ let%pworkflow summary samples bamstats chrstats =
     Option.value_map chrstats ~default:(txt "") ~f:(chrstat_table samples) ;
   ]
   in
-  Labs_croquis.Html_page.(
-    to_file
-      (make ~title:"Alignment summary" contents)
-      [%dest]
-  )
+  let bootstrap_head t =
+    let open Tyxml_html in
+    head (title (txt t)) [
+      link ~rel:[`Stylesheet] ~href:"http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" () ;
+      link ~rel:[`Stylesheet] ~href:"http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap-theme.min.css" () ;
+      script ~a:[a_src "https://code.jquery.com/jquery.js"] (txt "") ;
+      script ~a:[a_src "http://netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"] (txt "") ;
+    ]
+  in
+  let page ~title:t contents =
+    html (bootstrap_head t) (body [ div ~a:[a_class ["container"]] contents ])
+  in
+  let to_file doc fn =
+    Out_channel.with_file fn ~f:(fun oc ->
+        Tyxml_html.pp () (Format.formatter_of_out_channel oc) doc
+      )
+  in
+  to_file
+    (page ~title:"Alignment summary" contents)
+    [%dest]
